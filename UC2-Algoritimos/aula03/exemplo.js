6dab5b4278a03dde907366939dd07dd3f2d72782
Diulie Telles <121645164+diulietelles@users.noreply.github.com>
2024-03-08 21:35:02 -0300

    Add files via upload

---
+++ UC2-Algoritimos/aula03/exemplo.js
@@ -0,0 +1,16 @@
+//Este é um comentario de uma linha única
+/*
+este é 
+um comentario
+em varias
+linhas
+
+//Este comando imprime algo no terminal
+console.log('Olá Mundo!')*/
+const nome = "Diulie"
+const idade = 28
+console.log("Olá!", "Meu nome é", nome, "e eu tenho", idade, "anos")
+
+
+
+
---
+++ UC2-Algoritimos/aula03/exemplo2.js
@@ -0,0 +1,23 @@
+/*Numbers : são os tipos que representam números
+
+const idade1 = 23
+const altura = 1.79
+const temperatura = -20
+
+//Strings : representam conjunto de caracteres(texto)
+const nome = "Yuzo"
+let idade2 = "23"
+
+//Variaveis Booleanas: são variaveis que só assumem os valores TRUE or FALSE
+
+let souUmBoolean = true
+souUmBoolean = false
+*/
+
+//Exemplo 1
+
+const nome = "Diulie"
+const sobrenome = "Telles"
+const idade = 28
+let souEstudante = true
+console.log("Meu nome é", nome, sobrenome, "tenho", idade, "e ", "Sou estudante?", souEstudante )
