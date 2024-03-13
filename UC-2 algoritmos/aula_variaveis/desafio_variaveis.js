f5f35120c25dcd33f2133ee8535614a4a023bb76
Diulie Telles <121645164+diulietelles@users.noreply.github.com>
2024-03-13 10:10:10 -0300

    Add files via upload

---
+++ UC-2 algoritmos/aula_variaveis/desafio_variaveis.js
@@ -0,0 +1,12 @@
+// Solicitar dois números ao usuário e convertê-los em números
+
+// Solicita e converte o primeiro número
+var numero1 = Number(prompt("Digite o primeiro número:"))
+// Solicita e converte o segundo número
+var numero2 = Number(prompt("Digite o segundo número:")) 
+// Realizar as operações e imprimir os resultados
+
+ // Imprime a soma dos números
+console.log("1. O primeiro número mais osegundo número é:", numero1 + numero2)
+// Imprime a multiplicação dos números
+console.log("2. O primeiro número multiplicado pelo segundo número é:", numero1 * numero2) 
