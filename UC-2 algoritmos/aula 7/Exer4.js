fb3190ce55a102b4433669ac04f88862213e64ae
Diulie Telles <121645164+diulietelles@users.noreply.github.com>
2024-03-20 21:49:15 -0300

    Add files via upload

---
+++ UC-2 algoritmos/aula 7/Exer4.js
@@ -0,0 +1,15 @@
+let pokemon  = 'Bulbasauro'
+switch (pokemon) {
+    case 'Bulbasauro':
+        console.log('planta e veneno')
+        break
+    case 'Charmander':
+        console.log('fogo')
+        break
+    case 'Squirtle':
+        console.log('agua')
+        break
+    default:
+        console.log('pokemon nao encontrado')  
+        break  
+}
---
+++ UC-2 algoritmos/aula 7/condicionalComposta.js
@@ -0,0 +1,9 @@
+let idade = 16
+// if = se
+//se idade for maior ou igual a 18
+//executa o codigo
+if (idade >= 18){
+    console.log("Voce é maior de idade!")
+} else {
+    console.log("Voce é menor de idade!")
+}
